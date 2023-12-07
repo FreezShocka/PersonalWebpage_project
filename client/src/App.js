40d86828f61700173f8330563cc1445a1f6e3d94
FreezShocka <elliot.plagge@trojans.dsu.edu>
2023-12-07 04:16:50 -0600

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+
+function App() {
+
+  const [backendData, setBackEndData] = useState ([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackEndData(data)
+      }
+    )
+  }, [])
+  return (
+    <div>
+
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading...</p>
+      ): (
+        backendData.users.map((user, i) =>(
+          <p key={i}>{user}</p>
+        ))
+      )}
+
+    </div>
+  )
+}
+
+export default App
