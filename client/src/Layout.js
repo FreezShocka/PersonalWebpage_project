d013d5cd1ec451f94eca6b12dd7b827e5037fcc1
FreezShocka <elliot.plagge@trojans.dsu.edu>
2023-12-10 14:06:23 -0600

    IT'S WORKING!!!

--- client/src/Layout.js
+++ client/src/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Layout.css';
 
 const Layout = ({ children }) => {
   return (
@@ -7,6 +8,13 @@ const Layout = ({ children }) => {
       {/* Your hotbar goes here */}
       <header>
         {/* Hotbar content */}
+        <Link to="/">
+          <img
+          src="/Vanillite_pfp.png"
+          alt="Elliot Plagge"
+          className="layout-image"
+          />
+        </Link>
         <Link to="/about">About</Link>
         <Link to="/projects">Projects</Link>
         <Link to="/song">Song of the Week</Link>
--- client/src/Paths.js
+++ client/src/Paths.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './Home';
 import About from './About';
@@ -11,11 +11,11 @@ const Paths = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/song" component={Song} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/login" component={Login} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/song" element={<Song />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
